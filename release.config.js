e7c9098b9d423e7feb262f8b75c5b5f66b7c0ddc
Kuitos <kuitos.lau@gmail.com>
2025-06-19 11:36:26 +0800

    ci: fix sematic release config

--- release.config.js
+++ release.config.js
@@ -3,23 +3,10 @@
  */
 module.exports = {
   branches: [
-    '+([0-9])?(.{+([0-9]),x}).x',
     // Dynamic main branch configuration based on environment variable
     process.env.SEMANTIC_RELEASE_RC === 'true' 
       ? { name: 'main', prerelease: 'rc' }
       : 'main',
-    {
-      name: 'next',
-      prerelease: true
-    },
-    {
-      name: 'beta',
-      prerelease: true
-    },
-    {
-      name: 'alpha',
-      prerelease: true
-    }
   ],
   plugins: [
     [
