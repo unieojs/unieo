70c330343fe05b67e6b1c9fe036094538b1917e7
MadCcc <madccc@foxmail.com>
2025-07-31 16:54:20 +0800

    feat: redirect support value (#44)

--- src/core/meta/impl/RedirectMeta.ts
+++ src/core/meta/impl/RedirectMeta.ts
@@ -4,23 +4,38 @@ import { Redirect } from '../../Redirect';
 import type { RouteContext } from '../../RouteContext';
 import { BaseMeta } from '../BaseMeta';
 import type { BaseProcessor } from '../../processor';
+import type { ValueRawData } from '../../value';
+import { Value } from '../../value';
 
 export class RedirectMeta extends BaseMeta {
-  private readonly redirects: Redirect[];
+  private readonly redirects?: Redirect[];
+  private readonly redirectValue?: Value<RawRedirect[]>;
 
   constructor(options: {
     type: string;
     logger: ILogger;
     ctx: RouteContext;
-    data: RawRedirect[];
+    data: RawRedirect[] | ValueRawData;
     processor: BaseProcessor;
   }) {
     super(options);
-    this.redirects = options.data.map(raw => new Redirect(raw, options.processor));
+    if (Array.isArray(options.data)) {
+      this.redirects = options.data.map(raw => new Redirect(raw, options.processor));
+    } else {
+      this.redirectValue = new Value(options.data, options.processor);
+    }
   }
 
   public async process(): Promise<Response | undefined> {
-    for (const redirect of this.redirects) {
+    let redirects = this.redirects;
+    if (this.redirectValue) {
+      const rawRedirects = await this.redirectValue.get(this.ctx);
+      redirects = rawRedirects.map(raw => new Redirect(raw, this.processor));
+    }
+    if (!redirects || redirects.length === 0) {
+      return;
+    }
+    for (const redirect of redirects) {
       const res = await redirect.redirect(this.ctx);
       if (res) {
         // 重定向生效，跳过后续
@@ -30,6 +45,6 @@ export class RedirectMeta extends BaseMeta {
   }
 
   public needProcess(): boolean {
-    return this.redirects.length > 0;
+    return ((this.redirects?.length ?? 0) > 0) || !!this.redirectValue;
   }
 }
