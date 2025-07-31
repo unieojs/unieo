238e5263e9e36375b8d62e5d0c6fb76513345d39
MadCcc <madccc@foxmail.com>
2025-07-31 16:39:34 +0800

    feat: redirect support value

--- test/intergration/index.test.ts
+++ test/intergration/index.test.ts
@@ -41,6 +41,47 @@ describe('test/integration/index.text.ts', () => {
     assert.equal(response?.headers.get('Location'), 'https://www.example.com/');
   });
 
+  it('should redirect with value work', async () => {
+    const event = TestUtil.mockFetchEvent({
+      url: 'https://test.example.com/a',
+    });
+    const route = new Route({
+      event,
+    });
+    const response = await route.execute([
+      {
+        name: 'test',
+        type: 'test',
+        processor: GroupProcessorType.COMMON_GROUP_PROCESSOR,
+        meta: {},
+        args: {},
+        routes: [
+          {
+            name: 'sub2',
+            type: 'sub2',
+            processor: SubProcessorType.COMMON_SUB_PROCESSOR,
+            args: {},
+            meta: {
+              redirects: {
+                source: [
+                  {
+                    source: '/a',
+                    destination: 'https://www.example.com/',
+                    type: 'path',
+                  },
+                ],
+                sourceType: 'literal',
+                valueType: 'json',
+              },
+            },
+          },
+        ],
+      },
+    ]);
+    assert.equal(response?.status, 302);
+    assert.equal(response?.headers.get('Location'), 'https://www.example.com/');
+  });
+
   it('should requestRewrite work', async () => {
     fetchMock.get('https://www.example.com')
       .intercept({ method: 'get', path: '/' })
