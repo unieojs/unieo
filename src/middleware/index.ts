2306fdf8a7015ca61b3a72fcdedc22f361287cf8
MadCcc <madccc@foxmail.com>
2025-08-22 15:43:42 +0800

    chore: update

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,13 +1,7 @@
 import compose from 'koa-compose';
 import { ErrorCode, genError } from '../common/Error';
 import type { RouteContext } from '../core';
-import type {
-  Middleware,
-  MiddlewareConfig,
-  MiddlewareGen,
-  MiddlewareNext,
-  BaseMiddlewareOption,
-} from './types';
+import type { BaseMiddlewareOption, Middleware, MiddlewareConfig, MiddlewareGen, MiddlewareNext } from './types';
 import DefaultFetch from './DefaultFetch';
 import ErrorFallback from './ErrorFallback';
 
